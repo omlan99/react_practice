93cca74730843a0d0b9fb95d90f46636e9c8d18b
omlan99 <omlan10616@gmail.com>
2024-10-20 03:55:52 +0600

    second commit

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css'
 
 const Countries = () => {
-    const [countries, setContries] = useState([])
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setContries(data))
-    },[])
-    return (
-        <div>
-            Countries : {countries.length}
-            {
-                countries.map(country => <Country key={country.cca3} country ={country}></Country>)
-            }
-        </div>
-    );
+  const [countries, setContries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setContries(data));
+  }, []);
+  return (
+    <div>
+      Countries : {countries.length}
+      <div className="country-container">
+      {countries.map((country) => (
+        <Country key={country.cca3} country={country}></Country>
+      ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country
+    const {name, flags, population} = country
+    const [visited, setVisited] = useState(false)
+    // const [buttonText, setButtonText] = useState(Visit)
+    const handleVisited = () => {
+        setVisited(!visited)
+
+        // document.getElementById('visited').innerText = 'visit'
+    }
+    // const handleButtontext = () =>{
+    //     setButtonText(visted);
+    // }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <h2>Name : {name?.common}</h2>
             <img src={flags.png} alt="" />
+            <p>Population : {population}</p>
+            <button id='visited' onClick={handleVisited}>visit</button>
+            {visited ? "already visited" : 'Not visited'}
         </div>
     );
 };
